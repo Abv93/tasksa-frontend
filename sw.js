a889a97b7afb81117675edbc40531cb690eb1e98
Abv93 <albievisser2@gmail.com>
2026-08-05 22:05:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tasksa-v60';
+const CACHE = 'tasksa-v62';
 const STATIC = ['/','index.html','/manifest.json','/icon-192.png','/icon-512.png'];
 
 self.addEventListener('install', e => {
